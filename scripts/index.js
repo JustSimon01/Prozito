4640cca1118d4ce3b3034c665292e9fde07670ea
JustSimon01 <simon.ch@yandex.ru>
2022-07-12 21:46:44 +0300

    JS

--- scripts/index.js
+++ scripts/index.js
@@ -8,7 +8,5 @@ function openPopup(popup) {
       popup.classList.remove('popup_opened');
   } 
 
-  const aboutProject = document.querySelector('#aboutProject');
-  const headerMenu = document.querySelector('pop-up_menu').closest('.popup');
-  console.log(aboutProject);
-  console.log(headerMenu);
+
+  console.log(document);

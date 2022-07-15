8fc4c15b957c333a5db25884d2026db79cc78bfd
JustSimon01 <simon.ch@yandex.ru>
2022-07-15 17:22:48 +0300

    header adaptive

--- scripts/index.js
+++ scripts/index.js
@@ -10,11 +10,23 @@ function openPopup(popup) {
 
 //открытие-закрытие попапа меню через функции
 const button = document.querySelector('#aboutProject');
-const menuList = document.querySelector('.pop-up_menu');
+const menuList = document.querySelector('.header__menu');
 button.addEventListener('click', function(){
     if (menuList.classList.contains('pop-up_opend')){
         menuList.classList.remove('pop-up_opend');
     }else{
         menuList.classList.add('pop-up_opend');
     }
+});
+
+const menuBurgerButton = document.querySelector('.header__menu-icon');
+const menuPopup = document.querySelector('.header__menu-mobile');
+const menuPopupCloseButton = document.querySelector('.header__menu-icon_close');
+
+menuBurgerButton.addEventListener('click', function(){
+    menuPopup.classList.add('pop-up_opend');
+});
+
+menuPopupCloseButton.addEventListener('click', function(){
+    menuPopup.classList.remove('pop-up_opend');
 });
